38b6dd9164a6e7e11b54019c730c0cb1d7e2954b
Jesse Mulder <jesse@hacker115.nl>
2020-07-24 18:14:36 +0200

    New style :)

---
+++ js/run.js
@@ -0,0 +1,68 @@
+function copyNewUrl() {
+    /* Get the text field */
+    var copyText = document.getElementById("newUrl");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+}
+
+function isUrl(s) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return regexp.test(s);
+}
+
+function submit() {
+    $('#loader').removeClass("hidden");
+    $('#inputForm').addClass("hidden");
+    $('#successNotification').addClass("hidden");
+
+    if($('#urlToShorten').val() && isUrl($('#urlToShorten').val())){
+        $.ajax({
+            type: 'POST',
+            url: '/api/new',
+            data: JSON.stringify(
+                {
+                    url: $('#urlToShorten').val()
+                }
+            ),
+            success: function(data) {
+                alertify.success('De url is verkleind :)');
+                $('#inputForm').addClass("hidden");
+                $('#successNotification').removeClass("hidden");
+                $("#smallText").html('Nice, je URL is nu lekker kort :D');
+
+                $("#newUrl").val(data.Response.newUrl);
+                $('#loader').addClass("hidden");
+            },
+            error: function(data) {
+                alertify.error('data.responseJSON.Message');
+                UIkit.notification(data.responseJSON.Message, {pos: "bottom-left", status: 'danger'});
+                $('#loader').addClass("hidden");
+            },
+            contentType: "application/json",
+            dataType: 'json'
+        });
+    } else {
+        if ($('#urlToShorten').val()) {
+            alertify.warning('De waarde moet een url zijn! :)');
+        } else {
+            alertify.warning('De url mag niet leeg zijn :(');
+        }
+        $('#inputForm').removeClass("hidden");
+        $('#successNotification').addClass("hidden");
+        $("#smallText").html('Lekker snel een URL verkleinen?');
+        $('#loader').addClass("hidden");
+    }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const message = urlParams.get('message');
+if (message) {
+    alertify.error(message);
+}
+
+alertify.set('notifier','position', 'top-center');
--- staticRouter.js
+++ staticRouter.js
@@ -6,4 +6,12 @@ router.get('/img/:image', function (req, res) {
     res.sendFile(path.join(__dirname + '/img/' + req.params.image));
 });
 
+router.get('/css/:css', function (req, res) {
+    res.sendFile(path.join(__dirname + '/css/' + req.params.css));
+});
+
+router.get('/js/:js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/js/' + req.params.js));
+});
+
 module.exports = router;
