f2b1ac1adba1f480926592dd7dbab69508c4bf1d
Jesse Mulder <jesse@hacker115.nl>
2021-05-04 19:10:20 +0200

    Pre coding support for chrome/firefox extension

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $includeFunctions = @include 'includes/functions.php';
 if ($includeConfig && $includeFunctions) {
     removeExpiredSlugs();
 
-    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    if ($_SERVER['REQUEST_METHOD'] === "POST" || isset($_GET['url'])) {
         $errorMessage = true;
         $Result = [
             "type" => "success",
@@ -14,8 +14,16 @@ if ($includeConfig && $includeFunctions) {
             "message" => "Uw opgegeven URL is nu lekker kort."
         ];
 
-        if (filter_var($_POST['shortUrl'], FILTER_VALIDATE_URL)) {
-            if (strpos(strtolower($_POST['shortUrl']), "kort.app") !== false) {
+        if (isset($_GET['url'])) {
+            $validUrl = filter_var(base64_decode($_GET['url']), FILTER_VALIDATE_URL);
+        } else {
+            $validUrl = filter_var($_POST['shortUrl'], FILTER_VALIDATE_URL);
+        }
+
+        if ($validUrl) {
+            $url = (isset($_GET['url']) ? base64_decode($_GET['url']) : $_POST['shortUrl']);
+
+            if (strpos(strtolower($url), "kort.app") !== false) {
                 $Result["type"] = "danger";
                 $Result["title"] = "Whoops";
                 $Result["message"] = "Deze URL kan toch echt niet korter. Sorry";
@@ -27,12 +35,12 @@ if ($includeConfig && $includeFunctions) {
                     $slug = randomString();
                 }
 
-                if (!saveUrlAndSlug($slug, $_POST['shortUrl'], $editToken)) {
+                if (!saveUrlAndSlug($slug, $url, $editToken)) {
                     $Result["type"] = "danger";
                     $Result["title"] = "Whoops";
                     $Result["message"] = "Daar ging iets fout tijdens het opslaan van de URL. Probeer het later opnieuw.";
                 } else {
-                    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . strtok($_SERVER['REQUEST_URI'], '?');
                     $url = $actual_link . $slug;
                     $urlQR = urlencode($url);
                     $Result["message"] .= "<br/><br/><div class='uk-text-center'><img src='https://quickchart.io/qr?text=$urlQR&size=200' alt='QR Code' /><br/><br/><a class='uk-link' href='$url'>$url</a></div>";
